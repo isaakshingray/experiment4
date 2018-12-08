6c844efc342568cbbcd9b8da5be2ea1f0cf7ea09
isaakshingray <43245065+isaakshingray@users.noreply.github.com>
2018-12-07 22:00:56 -0500

    Add files via upload

---
+++ experiment4.ino
@@ -0,0 +1,21 @@
+
+int switch1val;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+}
+void loop() {
+  // read the switch value
+  switch1val = digitalRead(2);
+  // print out the switch value
+  Serial.write(switch1val);
+  
+
+
+ 
+  delay(100); 
+
+}
+
